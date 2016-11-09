2068c74cafb646ad928918207e3df91595c46b46
chakalmax <maximevanherzeele@hotmail.com>
2016-11-09 20:50:51 +0100

    Parseur fait. C'est une première version très basique

--- src/Tools/Parseur.java
+++ src/Tools/Parseur.java
@@ -1,6 +1,44 @@
 package Tools;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import DNA.Fragment;
+
 public class Parseur {
-	// YO, THIS IS A TEST FOR GIT MOTHERFUCKERS
+	// First version of Parseur
+
+	public ArrayList<Fragment> readFile(String fileName)
+	{
+		ArrayList<Fragment> parse = new ArrayList<Fragment>();
+		try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+		
+			int id=0;
+			String line = br.readLine();
+			while(line != null)
+			{
+				
+				if(line.startsWith(">")){
+					id++;
+					line = br.readLine();
+				}
+				Fragment frag = new Fragment(line,id);
+				parse.add(frag);
+				line = br.readLine();
+			}
+		
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("I/O exception");
+			e.printStackTrace();
+		}
+		
+		return parse;
+	}
  //TODO 
 }
